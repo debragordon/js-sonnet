41240cb7bfb412af5efcd9319f3a5be57f7608fc
Debra Gordon <debra.m.gordon@gmail.com>
2016-07-26 18:26:48 -0500

    finished assignment in class

--- sonnet.js
+++ sonnet.js
@@ -1,24 +1,25 @@
 // Paste the code below into your HTML file.  **DONE**
+var sonnet = document.getElementById("sonnet")
 
 // Take the contents of the sonnet div and place it in a variable
-var sonnetDiv = document.getElementById("sonnet").innerHTML;
-console.log(sonnetDiv);
+var sonnetDiv = sonnet.innerHTML;
+console.log("original sonnet: ", sonnetDiv);
 
 // Find and output the starting position of the word "orphans"
 var orphan = sonnetDiv.indexOf("orphans");
-console.log(orphan);
+console.log("starting position of orphans: ", orphan);
 
 // Output the number of characters in the sonnet
 var number = sonnetDiv.length;
-console.log(number);
+console.log("number of characters in the sonnet: ", number);
 
 // Replace the first occurance of the string "winter" with "yuletide"
 // Replace all occurances of the string "the" with "a large"
-var replace = sonnetDiv.replace(/the/g, "a large").replace(/winter/, "yuletide");
-console.log(replace);
+var replace = sonnetDiv.replace(/the /gi, "a large ").replace(/winter/, "yuletide");
+console.log("Replaced words: ", replace);
 
 // Set the content of the sonnet div with the new string
-document.getElementById("sonnet").innerHTML = replace;
-console.log(replace);
+sonnet.innerHTML = replace;
+console.log("I swapped out the original HTML on the page for my updated version: "replace);
 
 // I think I am finished with the assignment but I don't understand what I did
