23d99f866f6d56099780d55074092d5e7800f6c2
Debra Gordon <debra.m.gordon@gmail.com>
2016-07-25 21:13:54 -0500

    completed assignment and wrote the javascript

---
+++ sonnet.js
@@ -0,0 +1,22 @@
+// Paste the code below into your HTML file.  **DONE**
+
+// Take the contents of the sonnet div and place it in a variable
+var sonnetDiv = document.getElementById("sonnet").innerHTML;
+console.log(sonnetDiv);
+
+// Find and output the starting position of the word "orphans"
+var orphan = sonnetDiv.indexOf("orphans");
+console.log(orphan);
+
+// Output the number of characters in the sonnet
+var number = sonnetDiv.length;
+console.log(number);
+
+// Replace the first occurance of the string "winter" with "yuletide"
+// Replace all occurances of the string "the" with "a large"
+var replace = sonnetDiv.replace(/the/g, "a large").replace(/winter/, "yuletide");
+console.log(replace);
+
+// Set the content of the sonnet div with the new string
+document.getElementById("sonnet").innerHTML = replace;
+console.log(replace);
